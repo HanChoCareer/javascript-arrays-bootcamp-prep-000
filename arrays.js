ff937fee74dd34c157b07f485bb1f05151d1ff26
Hankyol Cho <hancho.career@gmail.com>
2018-06-20 14:34:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray (arr,elem){
   
 }
+function destructivelyAddElementToBeginningOfArray(arr, elem){
+  arr.unshift(elem);
+  return arr;
+}
